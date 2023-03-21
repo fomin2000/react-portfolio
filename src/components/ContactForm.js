69e6c5787b19a8586a0ce616d5d297c21850e9bd
Pasha Fomin <pashafomin@Pashas-MacBook-Pro.local>
2023-03-21 16:56:19 -0700

    more info

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,9 +6,11 @@ const ContactForm = () => {
         <div className="formCont">
             <h3>I'd love to connect with you! Here are some options...</h3>
             <ul>
-                <li>☎️ Call/Text: (916)513-8997</li>
-                <li></li>
-                <li></li>
+                <li>☎️<br />Call/Text: <a href="tel:9165138997">(916)513-8997</a></li>
+                <br />
+                <li>✉️<br />Email: <a href="mailto:pasha.fomin11@gmail.com">pasha.fomin11@gmail.com</a></li>
+                <br />
+                <li>💻<br /> GitHub: <a href="https://github.com/fomin2000">fomin2000</a></li>
             </ul>
         </div>
     )
